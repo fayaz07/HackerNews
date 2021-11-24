fa08dce82369ae77d942b74b2609b4203fe653e2
Mohammad Fayaz <fayazfz07@gmail.com>
2021-11-24 23:56:58 +0530

    added core splash screen api

--- app/src/main/java/com/mohammadfayaz/news/MainActivity.kt
+++ app/src/main/java/com/mohammadfayaz/news/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mohammadfayaz.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    installSplashScreen()
+
     setContentView(R.layout.activity_main)
   }
 }

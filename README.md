4c7949f56f3cfb7a71bc45bfcc8bf7f2039e321d
Mohammad Fayaz <fayazfz07@gmail.com>
2021-11-25 23:28:15 +0530

    added navhost fragment, fragment navigation using navController

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Android app consuming https://www.thenewsapi.com/ API
 ## About app
 
 1. Manages dependencies using Gradle's **version catalog and toml** config.
-
+2. Android Splash Screen API
+3. ViewBinding
+4. Navigation Components using Fragments
+5. LiveData & ViewModel
--- app/src/main/java/com/mohammadfayaz/news/MainActivity.kt
+++ app/src/main/java/com/mohammadfayaz/news/MainActivity.kt
@@ -3,13 +3,48 @@ package com.mohammadfayaz.news
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mohammadfayaz.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  private lateinit var navHostFragment: NavHostFragment
+  private lateinit var navController: NavController
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     installSplashScreen()
 
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    initFragmentHost()
   }
+
+  private fun initFragmentHost() {
+    navHostFragment =
+      supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+
+    navController = navHostFragment.navController
+
+    setupActionBarWithNavController(navController)
+
+    binding.bottomNavBar.setupWithNavController(navController)
+  }
+
+  override fun onBackPressed() {
+    if (!navController.navigateUp())
+      super.onBackPressed()
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    return navController.navigateUp() || super.onSupportNavigateUp()
+  }
+
 }

1e35c02d9e36701fe3da8df5daab2ee8393a20de
Mohammad Fayaz <fayazfz07@gmail.com>
2021-11-26 23:11:53 +0530

    added timber lib

--- app/src/main/java/com/mohammadfayaz/news/HackerNewsApp.kt
+++ app/src/main/java/com/mohammadfayaz/news/HackerNewsApp.kt
@@ -2,10 +2,15 @@ package com.mohammadfayaz.news
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class HackerNewsApp : Application() {
   override fun onCreate() {
     super.onCreate()
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+    }
   }
 }

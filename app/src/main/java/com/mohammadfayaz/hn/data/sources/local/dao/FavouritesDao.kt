46b6359b4431762d7f1359be37b7f1ae1faedc3c
Mohammad Fayaz <fayazfz07@gmail.com>
2022-01-13 16:17:53 +0530

    added favourites local source

--- app/src/main/java/com/mohammadfayaz/hn/data/sources/local/dao/FavouritesDao.kt
+++ app/src/main/java/com/mohammadfayaz/hn/data/sources/local/dao/FavouritesDao.kt
@@ -8,5 +8,8 @@ import com.mohammadfayaz.hn.domain.models.StoryType
 @Dao
 interface FavouritesDao : BaseDao<FavouriteModel> {
   @Query("SELECT * FROM favourites WHERE type = :type ORDER BY savedOn DESC")
-  suspend fun getAllIdsByType(type: StoryType): List<FavouriteModel>
+  suspend fun getAllByType(type: StoryType): List<FavouriteModel>
+
+  @Query("SELECT * FROM favourites WHERE id = :id")
+  suspend fun getById(id: Int): FavouriteModel?
 }
---
+++ app/src/main/java/com/mohammadfayaz/hn/data/sources/local/source/FavouritesLocalSource.kt
@@ -0,0 +1,25 @@
+package com.mohammadfayaz.hn.data.sources.local.source
+
+import com.mohammadfayaz.hn.data.sources.local.dao.FavouritesDao
+import com.mohammadfayaz.hn.domain.models.FavouriteModel
+import com.mohammadfayaz.hn.domain.models.StoryType
+import javax.inject.Inject
+
+class FavouritesLocalSource @Inject constructor(private val favouritesDao: FavouritesDao) :
+  BaseLocalSource() {
+  suspend fun isFavourite(id: Int): Boolean {
+    val exists = favouritesDao.getById(id)
+    return exists != null
+  }
+
+  suspend fun saveOrUnSave(id: Int, type: StoryType): Boolean {
+    val fav = favouritesDao.getById(id)
+    return if (fav != null) {
+      favouritesDao.delete(fav)
+      false
+    } else {
+      favouritesDao.insert(FavouriteModel(id, type))
+      true
+    }
+  }
+}

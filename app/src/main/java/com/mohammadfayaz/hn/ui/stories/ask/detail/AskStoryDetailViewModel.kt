45a7fec5eeb56b03bc2c383e2cf8ad905a626cb5
Mohammad Fayaz <fayazfz07@gmail.com>
2022-01-17 23:47:25 +0530

    added favourite use case to ask story detailed fragment

--- app/src/main/java/com/mohammadfayaz/hn/ui/stories/ask/detail/AskStoryDetailViewModel.kt
+++ app/src/main/java/com/mohammadfayaz/hn/ui/stories/ask/detail/AskStoryDetailViewModel.kt
@@ -1,14 +1,31 @@
 package com.mohammadfayaz.hn.ui.stories.ask.detail
 
+import androidx.lifecycle.viewModelScope
+import com.mohammadfayaz.hn.domain.models.StoryType
+import com.mohammadfayaz.hn.domain.usecases.favourites.CheckIsFavouriteUseCase
+import com.mohammadfayaz.hn.domain.usecases.favourites.SaveFavouriteUseCase
 import com.mohammadfayaz.hn.ui.base.BaseViewModel
+import com.mohammadfayaz.hn.ui.stories.ask.detail.AskStoryDetailFragment.Companion.FAVOURITE_EVENT
 import dagger.hilt.android.lifecycle.HiltViewModel
-import timber.log.Timber
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AskStoryDetailViewModel @Inject constructor() :
-  BaseViewModel() {
-  fun init() {
-    Timber.d("init")
+class AskStoryDetailViewModel @Inject constructor(
+  private val isFavouriteUseCase: CheckIsFavouriteUseCase,
+  private val saveFavouriteUseCase: SaveFavouriteUseCase
+) : BaseViewModel() {
+  fun isFavourite(id: Int) {
+    viewModelScope.launch {
+      val isFav = isFavouriteUseCase.invoke(id)
+      postSuccess(isFav, "", FAVOURITE_EVENT)
+    }
+  }
+
+  fun saveUnSaveAsFavourite(id: Int, storyType: StoryType) {
+    viewModelScope.launch {
+      val isFav = saveFavouriteUseCase.invoke(id, storyType)
+      postSuccess(isFav, "", FAVOURITE_EVENT)
+    }
   }
 }
